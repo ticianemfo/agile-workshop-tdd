5e2caddb5f08e6763ffe9157e507b3ba6e59a7f2
Ticiane <ticiane.ottoni@cgu.gov.br>
2012-01-26 11:45:13 -0200

    Inclusao de funcionalidade que seta uma fatura como paga

--- src/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletos.java
+++ src/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletos.java
@@ -5,9 +5,18 @@ import java.util.List;
 public class ProcessadorDeBoletos {
 
 	public void processa(List<Boleto> boletos, Fatura fatura) {
+		double valorPago=0;
 		for(Boleto boleto : boletos) {
 			Pagamento pagamento = new Pagamento(boleto.getValor(), MeioDePagamento.BOLETO);
 			fatura.getPagamentos().add(pagamento);
+			valorPago+=boleto.getValor();
 		}
+		quitaFatura(fatura, valorPago);
+	}
+	
+	private void quitaFatura (Fatura fatura, double valorPago){
+		if (fatura.getValor() <= valorPago)
+			fatura.setPago(true);
+		
 	}
 }
--- tests/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletosTest.java
+++ tests/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletosTest.java
@@ -1,6 +1,7 @@
 package br.com.caelum.tdd.exercicio3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -30,5 +31,15 @@ public class ProcessadorDeBoletosTest {
 		assertEquals(2, f.getPagamentos().size());
 		assertEquals(new Pagamento(200.0, MeioDePagamento.BOLETO), f.getPagamentos().get(0));
 		assertEquals(new Pagamento(500.0, MeioDePagamento.BOLETO), f.getPagamentos().get(1));
+	}
+	
+	@Test
+	public void deveQuitarAFaturaSeASomaDosBoletosMaiorOuIgualAoValorDaFatura(){
+		Fatura f = new Fatura("sun", 1000.0);
+		List<Boleto> boletosPagos = Arrays.asList(new Boleto(500.0), new Boleto(500.0));
+		
+		new ProcessadorDeBoletos().processa(boletosPagos, f);
+		assertTrue(f.isPago());
+		
 	}
 }
